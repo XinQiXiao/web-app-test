4519c4d99abed7452372dbaac38fe923cf015ffe
qixin <qixin@self.com>
2020-03-31 11:20:59 +0800

    create project

---
+++ src/router/MainRouter.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { Route, HashRouter, Switch, Redirect } from 'react-router-dom'
+
+// router
+import AdminRouter from './AdminRouter'
+
+// page
+import { HomePage } from '../page'
+
+class App extends Component{
+    render(){
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+export default class RouterComponent extends Component{
+    render(){
+        return (
+            <HashRouter>
+                <App>
+                    <Switch>
+                        <Route path="/" render={()=>
+                            <AdminRouter>
+                                <Switch>
+                                    <Route path="/" component={HomePage}/>
+                                </Switch>
+                            </AdminRouter>
+                        }/>
+                    </Switch>
+                </App>
+            </HashRouter>
+        )
+    }
+}
